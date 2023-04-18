06abd00ccf0471058f2a843d4ada0d61a7edbed5
888muba <mubamubashir888@gmail.com>
2023-04-19 05:29:08 +0530

    first commit

---
+++ js/dropdown.js
@@ -0,0 +1,20 @@
+/* Toggle between showing and hiding the dropdown content */
+
+function myFunction() {
+  
+    document.getElementById("myDropdown").classList.toggle("show");
+     
+  }
+  
+  // Close the dropdown if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      for (var i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
